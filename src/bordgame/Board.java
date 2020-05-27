31c99d72e38b52ec6e31f08e69b65f4f3316a83f
Alexandre Leao <erdleosalgueiro@gmail.com>
2020-05-27 16:31:11 -0300

    Moving pieces

--- src/bordgame/Board.java
+++ src/bordgame/Board.java
@@ -25,14 +25,14 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int column) {
-		if(!positionExistis(row, column)) {
+		if(!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
-		if(!positionExistis(position)) {
+		if(!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
@@ -48,20 +48,37 @@ public class Board {
 		
 	}
 	
-	private boolean positionExistis(int row, int column) {
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux  = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	private boolean positionExists(int row, int column) {
 	   return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 	
-	public boolean positionExistis(Position position) {
-		return positionExistis(position.getRow(), position.getColumn());
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
 		
 	}
 	
 	public boolean thereIsApiece(Position position) {
-		if(!positionExistis(position)) {
+		if(!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
 	}
+	
+	
 	
 }
